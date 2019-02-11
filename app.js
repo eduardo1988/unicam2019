181836fa08e1cea4a25fd0e7d6f3a203b6cd4e33
eduardo1988 <eduardo.romero@studenti.unicam.it>
2019-02-11 18:16:09 +0100

    creazione server e inizilizzaione

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("this is a simple example");
+console.log("start the server");
+
+const express = require('express');
+const app = express();
+
+// initialize the server
+app.listen(3000,function(){
+    console.log("live at Port 3000");
+});
