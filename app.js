6673bb5a73d60a6b09fc597b332dd7e2f02f12d4
eduardo1988 <eduardo.romero@studenti.unicam.it>
2019-02-12 09:08:08 +0100

    aggiunta della view engine EJS per gestire le VIEWS

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 console.log("start the server");
 
-const express = require('express');
-const app = express();
+const express = require('express'); // analogo alla import dipendenza  di java, dipendenza
+const app = express();      // viene istanziata express quindi il server
+
+app.set('view engine', 'ejs'); // view engine 'ejs'  sistema che gestirà le nostre viste
+
+
+
+app.get('/', function(req,res){
+    res.render('index',{user:"Great User", title:"homepage"});
+});
+
+
 
 // initialize the server
 app.listen(3000,function(){
