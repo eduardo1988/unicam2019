5a6e5ceaba69c470b932f1e450f61fe08600bcae
eduardo1988 <eduardo.romero@studenti.unicam.it>
2019-02-12 11:41:48 +0100

    aggiunta della view e handler di login page

--- app.js
+++ app.js
@@ -6,11 +6,24 @@ const app = express();      // viene istanziata express quindi il server
 app.set('view engine', 'ejs'); // view engine 'ejs'  sistema che gestirà le nostre viste
 
 
+/*
+*Ejs uses by defalut the views in the 'views' folder
+*/
 
 app.get('/', function(req,res){
-    res.render('index',{user:"Great User", title:"homepage"});
+    // var objPassedToView = {user:"Great User", title:"homepage"}
+    //res.render('index',objPassedToView);
+    // these two line above  are equivalent this line below 
+    res.render('index',{user:"Dido", title:"Test di esmepio"});
 });
 
+/**
+ * Create the handler for login page
+ */
+app.get('/login', function(req,res){
+    
+    res.render('login',{});
+});
 
 
 // initialize the server
