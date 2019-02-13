8d3fcbb03402bcc703e225964868619ff546fc13
eduardo1988 <eduardo.romero@studenti.unicam.it>
2019-02-14 00:32:01 +0100

    configurazione dell'handler app.post per gestire i dati provenienti dal form login.ejs

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 console.log("start the server");
 
 const express = require('express'); // analogo alla import dipendenza  di java, dipendenza
+var bodyParser = require('body-parser'); // per usufruire della dipendenza body-parser va istanziata
 const app = express();      // viene istanziata express quindi il server
-
+//CONFIGURAZIONI:
 app.set('view engine', 'ejs'); // view engine 'ejs'  sistema che gestirà le nostre viste
-
+app.use(bodyParser.json());     // dichiariamo cosa è necessario che deve accetare la mia app
+app.use(bodyParser.urlencoded({extended: true}));
 
 /*
  //Ejs uses by defalut the views in the 'views' folder
-
 app.get('/', function(req,res){
     // var objPassedToView = {user:"Great User", title:"homepage"}
     //res.render('index',objPassedToView);
     // these two line above  are equivalent this line below 
     res.render('index',{user:"Dido", title:"Test di esmepio"});
 });
-
 */ // codice commentato dopo aver stabilito chi sarà il nostro index, cioè la pagina iniziale
 
 
@@ -28,6 +28,17 @@ app.get('/', function(req,res){
      res.render('login',{});
 });
 
+// handler che gestirà il metodo POST del form login.ejs
+app.post('/login', function(req,res){
+    //console.log(req);
+    console.log(req.body);
+    user = req.body.email;
+    password = req.body.password;
+    session = req.session;
+    console.log(user,password);
+ 
+    
+});
 
 // initialize the server
 app.listen(3000,function(){
